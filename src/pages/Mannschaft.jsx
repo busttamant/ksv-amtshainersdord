7a42b7d9c3cd5b7c69d020911cdbd8208d2d07a1
busttamant <bustamanteruizdani@gmail.com>
2025-02-09 21:06:40 +0100

    Update Mannschaft.jsx

--- src/pages/Mannschaft.jsx
+++ src/pages/Mannschaft.jsx
@@ -7,6 +7,8 @@ export const Mannschaft = () => {
             { name: 'Steven Schlorf', position: 'Torwart', number: 1 },
             { name: 'Robert Schulz', position: 'Torwart', number: 22 },
             { name: 'Fin Herzog', position: 'Torwart', number: 99 },
+            { name: 'Maik Claus', position: 'Torwart', number: 67 },
+
         ],
         verteidiger: [
             { name: 'Daniel Bustamante', position: 'Verteidiger', number: 16 },
@@ -14,12 +16,16 @@ export const Mannschaft = () => {
             { name: 'Jhonny Vargas', position: 'Verteidiger', number: 5 },
             { name: 'Max Kraft', position: 'Verteidiger', number: 18 },
             { name: 'Robert Einert', position: 'Verteidiger', number: 2 },
+            { name: 'Max Grohmann', position: 'Verteidiger', number: 4 },
+
         ],
         mittelfeld: [
             { name: 'Stefan Schwanitz', position: 'Zentrales Mittelfeld', number: 8 },
             { name: 'Ezra Karabacak', position: 'Offensives Mittelfeld ', number: 15 },
             { name: 'Jonas Schwarzelt', position: 'Zentrales Mittelfeld', number: 4 },
-            { name: 'Marvin Lux', position: 'Offensives Mittelfeld ', number: 9 },
+            { name: 'Robert Durmis', position: 'Zentrales Mittelfeld', number: 81 },
+            { name: 'Arik May', position: 'Zentrales Mittelfeld', number: 14 },
+            { name: 'Marvin Lux', position: 'Offensives Mittelfeld ', number: 19 },
 
             { name: 'Leon Schönfelder', position: 'Zentrales Mittelfeld', number: 9 },
             { name: 'Toni Hoffmann', position: 'Zentrales Mittelfeld', number: 10 },
@@ -29,6 +35,7 @@ export const Mannschaft = () => {
             { name: 'Marlon Lange', position: 'Stürmer', number: 30 },
             { name: 'Valentin Voigt', position: 'Stürmer', number: 13 },
             { name: 'Leon Hauswald', position: 'Stürmer', number: 12 },
+            { name: 'Domenik Neumann', position: 'Stürmer', number: 38 },
         ],
     };
     // Estado para manejar la visibilidad de los jugadores adicionales en cada posición
